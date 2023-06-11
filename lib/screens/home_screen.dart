d36e48f30456605b59dc1f70098865f15245561e
Oleksandr <sanyagolikov97@gmail.com>
2023-06-11 17:42:02 +0300

    do: Successful request to the weather API

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,63 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_print
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:weather_app/model/weather.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var weatherService = WeatherService();
+  var weather = const Weather();
+
+  String currentWeather = '';
+  double tempC = 0;
+  double tempF = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getWeatherData();
+  }
+
+  void getWeatherData() async {
+    weather = await weatherService.getWeatherData("Ukraine");
+    setState(() {
+      currentWeather = weather.condition;
+      tempC = weather.temperatureC;
+      tempF = weather.temperatureF;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Wheather'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              currentWeather,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Text(
+              '${tempC.toString()} C',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Text(
+              '${tempF.toString()} F',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/services/weather_service.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/model/weather.dart';
+
+class WeatherService {
+  Future<Weather> getWeatherData(String place) async {
+    try {
+      final queryParameters = {
+        'key': '914b26eff8c04929a5c134248231106',
+        'q': place,
+      };
+
+      final url = Uri.http(
+        'api.weatherapi.com',
+        '/v1/current.json',
+        queryParameters,
+      );
+
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        return Weather.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception(
+            'Can not get wheather information, ${response.statusCode}');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
